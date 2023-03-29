e1cf8d7eca771fad7d1843eda7beec075d21f708
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-03-29 10:27:31 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 4 ms, Memory: 13156 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.rs
@@ -0,0 +1,14 @@
+use std::io::{stdin, BufRead, BufReader, BufWriter, Write};
+fn main() {
+  let stdin = stdin();
+  let mut reader = BufReader::new(stdin.lock());
+  let mut buffer = String::new();
+  reader.read_line(&mut buffer).unwrap();
+  let n: usize = buffer.trim().parse().unwrap();
+
+  let stdout = std::io::stdout();
+  let mut writer = BufWriter::new(stdout.lock());
+
+  writeln!(writer, "{}", usize::pow(n,2)).unwrap();
+  writeln!(writer, "{}",2).unwrap();
+}
