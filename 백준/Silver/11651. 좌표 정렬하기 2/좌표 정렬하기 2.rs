cd5552b28074997dc70ddfde5d831980a872a127
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-04-06 10:21:23 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 14580 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.rs
@@ -0,0 +1,34 @@
+use std::io::{stdin, BufRead, BufReader, BufWriter, Write};
+
+fn main() {
+    let stdin = stdin();
+    let mut reader = BufReader::new(stdin.lock());
+
+    let mut buffer = String::new();
+    reader.read_line(&mut buffer).unwrap();
+    let n = buffer.trim().parse::<usize>().unwrap();
+
+    let mut pairs: Vec<(i32, i32)> = Vec::new();
+    for _ in 0..n {
+        let mut buffer = String::new();
+        reader.read_line(&mut buffer).unwrap();
+        let mut nums = buffer.trim().split_whitespace().map(|x| x.parse::<i32>().unwrap());
+        let a = nums.next().unwrap();
+        let b = nums.next().unwrap();
+        pairs.push((a, b));
+    }
+
+    pairs.sort_by(|a, b| {
+        if a.1 == b.1 {
+            a.0.cmp(&b.0)
+        } else {
+            a.1.cmp(&b.1)
+        }
+    });
+
+    let stdout = std::io::stdout();
+    let mut writer = BufWriter::new(stdout.lock());
+    for (a, b) in pairs {
+        write!(writer, "{} {}\n", a, b).unwrap();
+    }
+}
