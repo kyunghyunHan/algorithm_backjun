4aeeeed783d1305a335d8e5d23d0c56fc6f82289
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-04-10 10:15:07 +0900

    [Bronze I] Title: 최소공배수, Time: 4 ms, Memory: 13156 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.rs
@@ -0,0 +1,35 @@
+use std::io::{stdin, BufRead, BufReader, BufWriter, Write};
+
+
+
+
+
+
+fn gcd(x: usize, y: usize) -> usize {
+  if y == 0 {
+      x // y 대신 x를 반환하도록 수정
+  } else {
+      gcd(y, x % y)
+  }
+}
+fn lcm (x:usize,y:usize)->usize{
+  (x * y) / gcd(x, y)
+}
+fn main() {
+  let stdin = stdin();
+  let mut reader = BufReader::new(stdin.lock());
+  let mut buffer = String::new();
+  reader.read_line(&mut buffer).unwrap();
+  let n: usize = buffer.trim().parse().unwrap();
+  let stdout = std::io::stdout();
+  let mut writer = BufWriter::new(stdout.lock());
+
+  for i in 0..n {
+      let mut buffer2 = String::new();
+      reader.read_line(&mut buffer2).unwrap(); // 수정된 부분
+      let mut nums = buffer2.trim().split_whitespace();
+      let x = nums.next().unwrap().parse::<usize>().unwrap();
+      let y = nums.next().unwrap().parse::<usize>().unwrap();
+      writeln!(writer, "{}", lcm(x, y)).unwrap();
+  }
+}
