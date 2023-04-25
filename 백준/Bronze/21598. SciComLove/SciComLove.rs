29a604beff8fe84c281b2d3b06bc319d820bd75d
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-04-25 14:26:22 +0900

    [Bronze V] Title: SciComLove, Time: 4 ms, Memory: 13156 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.rs
@@ -0,0 +1,20 @@
+use std::io::{BufRead,BufReader,BufWriter,Write,stdin,stdout};
+use std::cmp;
+fn main(){
+  let mut reader= BufReader::new(stdin().lock());
+
+
+  let mut writer= BufWriter::new(stdout().lock());
+  let mut input= String::new();
+    reader.read_line(&mut input).unwrap();
+    let mut nums= input.trim().split_whitespace().map(|x|x.parse::<usize>().unwrap());
+    let a= nums.next().unwrap();
+   
+
+   for i in 1..=a{
+      writeln!(writer,"SciComLove").unwrap();
+   }
+ 
+  
+
+}
