2be93f587af8ef964afb5441dc5e2e409f43453d
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-04-25 15:05:49 +0900

    [Bronze V] Title: 나는 누구인가, Time: 8 ms, Memory: 13148 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.rs
@@ -0,0 +1,8 @@
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+
+fn main() {
+    let reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout());
+
+    writeln!(writer, "{}", "I'm Sexy").unwrap();
+}
