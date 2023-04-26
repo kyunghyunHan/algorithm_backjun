7f34eccd73e4514e17a3c5537e16efb37e3552bc
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-04-26 12:49:06 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 4 ms, Memory: 13148 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.rs
@@ -0,0 +1,15 @@
+use std::io::{BufWriter, BufReader, BufRead, Write, stdin, stdout};
+use std::cmp;
+
+fn main() {
+    let mut reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout().lock());
+ 
+   
+
+      writeln!(writer,"{}","고려대학교").unwrap();
+
+    
+
+    
+}
