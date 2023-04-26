44ff65bcb1dd88509b627c50a08fe027d0158dee
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-04-26 13:24:00 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 8 ms, Memory: 13148 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    let n: i32 = input.trim().parse().expect("Failed to parse input");
+
+    for i in 1..=n {
+        for _ in 1..i {
+            print!(" ");
+        }
+        for _ in 1..=n - i + 1 {
+            print!("*");
+        }
+        println!("");
+    }
+}
