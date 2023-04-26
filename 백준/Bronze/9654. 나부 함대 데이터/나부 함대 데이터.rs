84452e7d913351b16dc00d74fb9623600ac1f17c
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-04-26 18:08:05 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 4 ms, Memory: 13140 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+    println!("N2 Bomber      Heavy Fighter  Limited    21        ");
+    println!("J-Type 327     Light Combat   Unlimited  1         ");
+    println!("NX Cruiser     Medium Fighter Limited    18        ");
+    println!("N1 Starfighter Medium Fighter Unlimited  25        ");
+    println!("Royal Cruiser  Light Combat   Limited    4        ");
+}
