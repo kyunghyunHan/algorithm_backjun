f167be716aa4f8e89254e0d0ff5debaba4f83809
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-06-01 11:09:34 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 4 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.rs
@@ -0,0 +1,13 @@
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+
+fn main(){
+let mut reader= BufReader::new(stdin().lock());
+let mut input = String::new();
+
+reader.read_line(&mut input).unwrap();
+let mut writer= BufWriter::new(stdout().lock());
+
+writeln!(writer,"{}","A");
+
+
+}
