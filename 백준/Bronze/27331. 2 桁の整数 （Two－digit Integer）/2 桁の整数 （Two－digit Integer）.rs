8c84d6f3dcde179020ca14f4862b3b8d172e50e0
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-06-05 10:21:46 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 4 ms, Memory: 13156 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.rs
@@ -0,0 +1,16 @@
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+fn main(){
+
+    let mut reader= BufReader::new(stdin().lock());
+    let mut input = String::new();
+    let mut writer= BufWriter::new(stdout().lock());
+    reader.read_line(&mut input).unwrap();
+   let n :u128= input.trim().parse().unwrap();
+   let mut result =1;
+   input.clear();
+   reader.read_line(&mut input).unwrap();
+   let n2 :u128= input.trim().parse().unwrap();
+   writeln!(writer,"{}{}",n,n2).unwrap();
+
+
+}
