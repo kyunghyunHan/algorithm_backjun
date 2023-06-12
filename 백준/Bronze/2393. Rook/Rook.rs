64cdc5045221a603d6b1d655c18f1ac46e88484b
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-06-12 10:21:19 +0900

    [Bronze V] Title: Rook, Time: 4 ms, Memory: 13140 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.rs
@@ -0,0 +1,19 @@
+use std::io::{BufReader,BufWriter,BufRead,Write,stdin,stdout};
+fn main(){
+
+    
+
+println!("  ___  ___  ___");
+println!("  | |__| |__| |");
+println!("  |           |");
+println!("   \\_________/");
+println!("    \\_______/");
+println!("     |     |");
+println!("     |     |");
+println!("     |     |");
+println!("     |     |");
+println!("     |_____|");
+println!("  __/       \\__");
+println!(" /             \\");
+println!("/_______________\\")
+}
