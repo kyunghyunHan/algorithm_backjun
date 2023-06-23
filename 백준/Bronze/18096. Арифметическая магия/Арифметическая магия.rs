f5cd9d628785d16c6146fdefb1e3175d0e954934
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-06-23 17:41:19 +0900

    [Bronze V] Title: Арифметическая магия, Time: 4 ms, Memory: 13156 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.rs
@@ -0,0 +1,13 @@
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+
+fn main(){
+    let mut reader= BufReader::new(stdin().lock());
+    let mut writer= BufWriter::new(stdout().lock());
+    let mut input = String::new();
+    reader.read_line(&mut input).unwrap();
+    let a:i32= input.trim().parse().unwrap();
+     
+     
+   
+    writeln!(writer,"{}",1).unwrap();
+}
