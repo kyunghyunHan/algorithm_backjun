d7b420cbe40a01a4f2c9cdd64ea74afb0e695ec2
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-06-26 10:20:15 +0900

    [Bronze V] Title: A+B - 9, Time: 4 ms, Memory: 13160 KB -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.rs
@@ -0,0 +1,11 @@
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+
+fn main(){
+    let mut reader= BufReader::new(stdin().lock());
+    let mut writer= BufWriter::new(stdout().lock());
+    let mut input = String::new();
+    reader.read_line(&mut input).unwrap();
+    let mut nums:Vec<i128>= input.trim().split_whitespace().map(|x|x.parse().unwrap()).collect();  
+  
+    writeln!(writer,"{}",nums[0]+nums[1]).unwrap();
+}
