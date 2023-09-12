77aaa38874993a325abd42ba451dc513920f0ba5
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-09-12 10:27:07 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 13156 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.rs
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.rs
@@ -1,21 +1,14 @@
-//별찍기
-use std::fmt::Write;
-use std::io;
-fn main() {
-    let mut numbers_arry = String::new();
-
-    io::stdin().read_line(&mut numbers_arry).unwrap();
-
-    let numbers: Vec<&str> = numbers_arry.split_whitespace().collect();
-
-    let number: i32 = numbers[0].parse::<i32>().unwrap();
-
-    for i in 1..number + 1 {
-        for j in 1..i + 1 {
-            if j <= i {
-                print!("*");
-            }
+use std::io::{BufReader,BufWriter,BufRead,Write,stdin,stdout};
+fn main(){
+    let mut reader= BufReader::new(stdin().lock());
+    let mut writer= BufWriter::new(stdout().lock());
+    let mut input = String::new();
+    reader.read_line(&mut input).unwrap();
+    let num = input.trim().parse::<i32>().unwrap();
+    for i in 0..num{
+        for j in 0..=i{
+            write!(writer,"{}","*").unwrap();
         }
-        print!("\n");
+        writeln!(writer).unwrap();
     }
 }
