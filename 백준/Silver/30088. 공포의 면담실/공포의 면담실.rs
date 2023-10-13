ee6024a1111ec5a0319ed6628da51783dfb4da82
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-10-13 11:08:48 +0900

    [Silver V] Title: 공포의 면담실, Time: 48 ms, Memory: 13420 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/30088. 공포의 면담실/공포의 면담실.rs
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.rs
@@ -4,22 +4,22 @@ fn main(){
     let mut writer= BufWriter::new(stdout().lock());
     let mut input = String::new();
     reader.read_line(&mut input).unwrap();
-    let mut v:Vec<i32>= Vec::new();
+    let mut v:Vec<usize>= Vec::new();
      let n= input.trim().parse().unwrap();
      for _ in 0..n{
          input.clear();
          reader.read_line(&mut input).unwrap();
-         let t: Vec<i32> = input
+         let t: Vec<usize> = input
             .split_whitespace()
-            .map(|s| s.parse().expect("Invalid input"))
+            .map(|s| s.parse().unwrap())
             .collect();
              v.push(t.iter().skip(1).sum())
      }
      v.sort();
      let mut answer = 0;
-
+     
      for i in 0..n {
-         answer += (n - i) as i32 * v[i];
+         answer += (n - i) * v[i];
 
      }
 
