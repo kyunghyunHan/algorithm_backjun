cfc28ff02ce3ed40363897b9cd566baddedcd402
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-10-27 09:51:05 +0900

    [Silver V] Title: 단어 정렬, Time: 20 ms, Memory: 15444 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.rs
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.rs
@@ -1,5 +1,34 @@
-use std::io::{self, BufRead};
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
 use std::cmp::Ordering;
+fn main(){
+ 
+    let mut reader= BufReader::new(stdin().lock());
+    let mut writer=BufWriter::new(stdout().lock());
+    let mut input = String::new();
+    let mut word: Vec<String> = vec![];
+    reader.read_line(&mut input).unwrap();
+    let n:i32=input.trim().parse().unwrap();
+
+       // word 배열 입력받기
+    for _ in 0..n {
+       input.clear();
+       reader.read_line(&mut input).unwrap();
+        word.push(input.trim().to_string());
+    }
+
+    word.sort_by(cmp);
+    let mut prev_word = "";
+
+    for w in word.iter() {
+        if w != prev_word {
+        writeln!(writer,"{}", w).unwrap();
+        prev_word = w;
+        }
+    }
+
+     writer.flush().unwrap();
+    
+}
 
 fn cmp(a: &String, b: &String) -> Ordering {
     // 1. 길이가 같다면, 사전순으로
@@ -9,34 +38,5 @@ fn cmp(a: &String, b: &String) -> Ordering {
     // 2. 길이가 다르다면, 짧은 순으로
     else {
         return a.len().cmp(&b.len());
-    }
-}
-
-fn main() {
-    let stdin = io::stdin();
-    let mut word: Vec<String> = vec![];
-
-    // N 입력받기
-    let mut input = String::new();
-    stdin.lock().read_line(&mut input).unwrap();
-    let n: usize = input.trim().parse().unwrap();
-
-    // word 배열 입력받기
-    for _ in 0..n {
-        let mut input = String::new();
-        stdin.lock().read_line(&mut input).unwrap();
-        word.push(input.trim().to_string());
-    }
-
-    // word 정렬하기
-    word.sort_by(cmp);
-
-    // 중복 제거 후 출력
-    let mut prev_word = "";
-    for w in word.iter() {
-        if w != prev_word {
-            println!("{}", w);
-            prev_word = w;
-        }
     }
 }
