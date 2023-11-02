f08972d731d61a524be5811ed8792d4fb350dc27
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-02 09:48:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 48 ms, Memory: 14636 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.rs
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.rs
@@ -1,27 +1,33 @@
-use std::io::{stdin, BufRead, BufReader, BufWriter, Write};
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+fn main(){
+  let mut reader= BufReader::new(stdin().lock());
+  let mut writer= BufWriter::new(stdout().lock());
+  let mut input: String = String::new();
+  reader.read_line(&mut input).unwrap();
+  //점의 개수
+  let n:i32= input.trim().parse().unwrap();
 
-fn main() {
-    let stdin = stdin();
-    let mut reader = BufReader::new(stdin.lock());
+  //배열선언
+  let mut  v:Vec<(i32,i32)>= Vec::new();
+  for _ in 0..n{
+    input.clear();
+    reader.read_line(&mut input).unwrap();
+    let nums:Vec<i32>= input.trim().split_whitespace().map(|x|x.parse().unwrap()).collect();
+    //좌표값 배열에 집어 넣기
+    v.push((nums[0],nums[1]))
+  }
 
-    let mut buffer = String::new();
-    reader.read_line(&mut buffer).unwrap();
-    let n = buffer.trim().parse::<usize>().unwrap();
-
-    let mut pairs: Vec<(i32, i32)> = Vec::new();
-    for _ in 0..n {
-        let mut buffer = String::new();
-        reader.read_line(&mut buffer).unwrap();
-        let mut nums = buffer.trim().split_whitespace().map(|x| x.parse::<i32>().unwrap());
-        let a = nums.next().unwrap();
-        let b = nums.next().unwrap();
-        pairs.push((a, b));
+  v.sort_by(|a,b|{
+    if a.0==b.0{
+      a.1.cmp(&b.1)
+    }else{
+      a.0.cmp(&b.0)
     }
+  });
+  for (a, b) in v {
+    writeln!(writer, "{} {}", a, b).unwrap();
+}
 
-    pairs.sort();
-    let stdout = std::io::stdout();
-    let mut writer = BufWriter::new(stdout.lock());
-    for (a, b) in pairs {
-        write!(writer, "{} {}\n", a, b).unwrap();
-    }
+   writer.flush().unwrap();
+
 }
