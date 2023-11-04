7bd932a77b230845ec74878360289750a9c81bbb
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-04 14:15:37 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 4 ms, Memory: 13212 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.rs
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.rs
@@ -1,5 +1,3 @@
-use std::io::{self, BufRead};
-
 const WB: [&str; 8] = [
     "WBWBWBWB",
     "BWBWBWBW",
@@ -22,53 +20,58 @@ const BW: [&str; 8] = [
     "WBWBWBWB"
 ];
 
-fn wb_cnt(x: usize, y: usize, board: &[String]) -> usize {
-    let mut cnt = 0;
-    for i in 0..8 {
-        for j in 0..8 {
-            if board[x+i].chars().nth(y+j) != WB[i].chars().nth(j) {
-                cnt += 1;
+use core::num;
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+
+fn main(){
+  let mut reader= BufReader::new(stdin().lock());
+  let mut writer= BufWriter::new(stdout().lock());
+
+  let mut input = String::new();
+  reader.read_line(&mut input).unwrap();
+  let nums= input.trim().split_ascii_whitespace().map(|x|x.parse().unwrap()).collect::<Vec<usize>>();
+  let n= nums[0];
+  let m = nums[1];
+  let mut board = Vec::new();
+    let mut result = Vec::new();
+
+    for _ in 0..n {
+        input.clear();
+        reader.read_line(&mut input).unwrap();
+        board.push(input.trim().to_string());
+    }
+
+    for i in 0..n - 7 {
+        for j in 0..m - 7 {
+            let mut draw1 = 0;
+            let mut draw2 = 0;
+
+            for a in i..i + 8 {
+                for b in j..j + 8 {
+                    if (a + b) % 2 == 0 {
+                        if board[a].as_bytes()[b] as char != 'B' {
+                            draw1 += 1;
+                        }
+                        if board[a].as_bytes()[b] as char != 'W' {
+                            draw2 += 1;
+                        }
+                    } else {
+                        if board[a].as_bytes()[b] as char != 'W' {
+                            draw1 += 1;
+                        }
+                        if board[a].as_bytes()[b] as char != 'B' {
+                            draw2 += 1;
+                        }
+                    }
+                }
             }
+
+            result.push(draw1);
+            result.push(draw2);
         }
     }
-    cnt
+
+    
+  writeln!(writer,"{:?}",result.iter().min().unwrap()).unwrap();
 }
 
-fn bw_cnt(x: usize, y: usize, board: &[String]) -> usize {
-    let mut cnt = 0;
-    for i in 0..8 {
-        for j in 0..8 {
-            if board[x+i].chars().nth(y+j) != BW[i].chars().nth(j) {
-                cnt += 1;
-            }
-        }
-    }
-    cnt
-}
-
-fn main() {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines().map(|line| line.unwrap());
-    let p1: Vec<usize> = lines
-        .next()
-        .unwrap()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-    let mut board: Vec<String> = Vec::new();
-    for _ in 0..p1[0] {
-        board.push(lines.next().unwrap());
-    }
-
-    let mut min_val = usize::MAX;
-    for i in 0..(p1[0]-8+1) {
-        for j in 0..(p1[1]-8+1) {
-            let tmp = std::cmp::min(wb_cnt(i, j, &board), bw_cnt(i, j, &board));
-            if tmp < min_val {
-                min_val = tmp;
-            }
-        }
-    }
-
-    println!("{}", min_val);
-}
