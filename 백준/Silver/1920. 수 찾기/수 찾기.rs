aafc9aad1c739544a5b50344641d788c1d98c183
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-06 10:13:29 +0900

    [Silver IV] Title: 수 찾기, Time: 32 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.rs
+++ 백준/Silver/1920. 수 찾기/수 찾기.rs
@@ -9,6 +9,7 @@ fn main(){
     let n:i64= input.trim().parse().unwrap();
     input.clear();
     reader.read_line(&mut input).unwrap();
+    
     let a: HashSet<i64> = input
     .split_whitespace()
     .map(|s| s.parse().unwrap())
@@ -19,11 +20,17 @@ fn main(){
     input.clear();
     reader.read_line(&mut input).unwrap();
     let v= input.trim().split_whitespace().map(|x|x.parse().unwrap()).collect::<Vec<i64>>();
-    
+    //Vec로는 속도가 느려서 시간제한이 걸림
+    //HashSet함수로 사용해서 해야함
+    //배열안에 값이 중복이지 않으며 탐색하기에 적합
     for &num in &v {
+        //a의 배열안에 v[i]의 값이 잇는지 확인
         if a.contains(&num) {
+            //있으면1
             writeln!(writer,"1").unwrap()
         } else {
+           //없으면 0
+
             writeln!(writer,"0").unwrap()
         }
     }
