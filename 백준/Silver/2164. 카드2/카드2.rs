89215b1ca8692bb301605c08b4d194f7ee509909
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-07 09:42:55 +0900

    [Silver IV] Title: 카드2, Time: 8 ms, Memory: 17120 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.rs
+++ 백준/Silver/2164. 카드2/카드2.rs
@@ -1,30 +1,29 @@
 use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
-use std::collections::VecDeque;
-
+use std::collections:: VecDeque;
 fn main(){
     let mut reader= BufReader::new(stdin().lock());
-
+    let mut writer= BufWriter::new(stdout().lock());
     let mut input = String::new();
-
     reader.read_line(&mut input).unwrap();
+    //n장의 카
+    let n:u64= input.trim().parse().unwrap();
+    //1번 카드가 젤  위에
+    //n번 카드가 젤 아래
+    let mut v: VecDeque<u64> = (1..=n).collect();
+    while v.len()>1 {
+        if  v.len()<1 {
+        writeln!(writer,"{}",v.front().unwrap()).unwrap();
+        break;
 
-    let mut nums= input.trim().split_whitespace().map(|x|x.parse::<usize>().unwrap());
-let mut writer= BufWriter::new(stdout().lock());
-    let mut n= nums.next().unwrap();
-    
-    let mut q: VecDeque<usize> = (1..=n).collect();
+       }
+       //맨 앞 삭제
+       v.pop_front();
+       //삭제 후 
+       let front = v.pop_front().unwrap();
+       //뒤에 집어넣기
+       v.push_back(front);
+    }
+    writeln!(writer,"{}",v.front().unwrap()).unwrap();
 
-
-
-
-   while q.len()>1 {
-    q.pop_front();
-    let front = q.pop_front().unwrap();
-
-   q.push_back(front);
-       
-   }
-   writeln!(writer,"{}",q.front().unwrap()).unwrap();
+  writer.flush().unwrap();
 }
-//요세푸스문제
-//1부터 n까지 q에 넣고 맨앞의 수를 뺴서 맨뒤로 보내는 과정을 반복하여 마닞막에 큐에 남는 수를 출력
