edcfeafa300e03076522050ec78e2f7c0e2dc1ee
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-10 09:38:58 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 13212 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.rs
+++ 백준/Silver/9012. 괄호/괄호.rs
@@ -1,11 +1,33 @@
-use std::io::{self, BufRead};
-
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+fn main(){
+    let mut reader= BufReader::new(stdin().lock());
+    let mut writer= BufWriter::new(stdout().lock());
+    let mut input = String::new();
+    reader.read_line(&mut input).unwrap();
+    let n= input.trim().parse().unwrap();
+    for _  in 0..n{
+      input.clear();
+      reader.read_line(&mut input).unwrap();
+      let s= input.trim();
+      if is_balanced(&s) {
+        writeln!(writer,"YES").unwrap();
+    } else {
+      writeln!(writer,"NO").unwrap();
+    }
+    }
+    writer.flush().unwrap();
+}
 fn is_balanced(s: &str) -> bool {
     let mut stack: Vec<char> = Vec::new();
+    //문자열 순회
     for ch in s.chars() {
+      //스택이 비어있어야함
+      //(이거면 추가
         if ch == '(' {
             stack.push(ch);
+             //)이거면 삭제
         } else if ch == ')' {
+          //Vec이 비어있으면 순회가 끝나기 전에 비어 있으면 한쪽으로 기울엇다 판단하고 false
             if stack.is_empty() {
                 return false;
             } else {
@@ -13,20 +35,6 @@ fn is_balanced(s: &str) -> bool {
             }
         }
     }
+    //Vec이 비어있으면 true를 반환
     stack.is_empty()
-}
-
-fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    let mut lines = stdin.lock().lines();
-    let n: usize = lines.next().unwrap()?.trim().parse().unwrap();
-    for _ in 0..n {
-        let s = lines.next().unwrap()?.trim().to_string();
-        if is_balanced(&s) {
-            println!("YES");
-        } else {
-            println!("NO");
-        }
-    }
-    Ok(())
 }
