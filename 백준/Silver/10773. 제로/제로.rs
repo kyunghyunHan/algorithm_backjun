38bbae783d5f9bb236e516a1fc25f8f7083c641d
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-10 20:45:37 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.rs
+++ 백준/Silver/10773. 제로/제로.rs
@@ -1,39 +1,22 @@
-use std::io::{stdin, BufRead, BufReader, BufWriter, Write};
-fn main() {
-     let stdin = stdin();
-    let mut reader = BufReader::new(stdin.lock());
-  
-    let mut buffer = String::new();
-    reader.read_line(&mut buffer).unwrap(); // 수정된 부분
-    let mut nums = buffer.trim().split_whitespace();
-    let n = nums.next().unwrap().parse::<usize>().unwrap();
-    let stdout = std::io::stdout();
-    let mut writer = BufWriter::new(stdout.lock());
-    let mut s: Vec<usize> = vec![];
-    for i in 0..n {
-        let mut buffer = String::new();
-    reader.read_line(&mut buffer).unwrap(); // 수정된 부분
-    let mut nums = buffer.trim().split_whitespace();
-    let n = nums.next().unwrap().parse::<usize>().unwrap();
-  
+use std::io::{BufReader,BufWriter,BufRead,Write,stdin,stdout};
 
-    if n ==0{
-        s.pop();
-    }
-    else{
-        s.push(n);
-    }
-
-
-
-
-    }
-   
-   let mut result =0;
-   for i in s{
-       result +=i;
-   }
-
-   writeln!(writer,"{}",result).unwrap();
-
+fn main(){
+  let mut reader= BufReader::new(stdin().lock());
+  let mut writer= BufWriter::new(stdout().lock());
+  let mut input = String::new();
+  reader.read_line(&mut input).unwrap();
+  let n= input.trim().parse().unwrap();
+  let mut v= Vec::new();
+  for _ in 0..n{
+      input.clear();
+      reader.read_line(&mut input).unwrap();
+      let n:u64= input.trim().parse().unwrap();
+      if n==0 {
+        v.pop();
+      }else{
+        v.push(n);
+      }
+  }
+let result:u64= v.iter().sum();
+  writeln!(writer,"{}",result).unwrap();
 }
