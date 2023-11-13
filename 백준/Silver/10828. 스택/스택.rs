a70f37f8034d8e3dce1c2d310211e9e20d32474c
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-13 09:27:51 +0900

    [Silver IV] Title: 스택, Time: 4 ms, Memory: 13216 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.rs
+++ 백준/Silver/10828. 스택/스택.rs
@@ -1,51 +1,53 @@
-use std::io::{stdin, BufRead, BufReader};
+use std::io::{BufReader,BufWriter,BufRead,Write,stdin,stdout};
+fn main(){
+  let mut reader= BufReader::new(stdin().lock());
+  let mut writer= BufWriter::new(stdout().lock());
+  let mut input = String::new();
+  reader.read_line(&mut input).unwrap();
+  //명령의 수
+  let n:i64= input.trim().parse().unwrap();
+  let mut s: Vec<i32> = vec![];
+  for _ in 0..n {
+      input.clear();
+      reader.read_line(&mut input).unwrap(); // 수정된 부분
+      let mut nums = input.trim().split_whitespace();
+      let n = nums.next().unwrap();
 
-fn main() {
-    let stdin = stdin();
-    let mut reader = BufReader::new(stdin.lock());
-    let mut buffer = String::new();
-    reader.read_line(&mut buffer).unwrap(); // 수정된 부분
-    let mut nums = buffer.trim().split_whitespace();
-    let n = nums.next().unwrap().parse::<usize>().unwrap();
-    let mut s: Vec<i32> = vec![];
-    for _ in 0..n {
-        let mut buffer = String::new();
-        reader.read_line(&mut buffer).unwrap(); // 수정된 부분
-        let mut nums = buffer.trim().split_whitespace();
-        let n = nums.next().unwrap();
+      // 1. push
+      if n == "push" {
+          let n2 = nums.next().unwrap().parse::<i32>().unwrap();
+          s.push(n2);
+      }
+      // 2. pop
+      else if n == "pop" {
+          if let Some(x) = s.pop() {
+              writeln!(writer,"{}", x).unwrap();
+          } else {
+            writeln!(writer,"{}", -1).unwrap();
+          }
+      }
+      // 3. size
+      else if n == "size" {
+        writeln!(writer,"{}", s.len()).unwrap();
+      }
+      // 4. empty
+      else if n == "empty" {
+          if s.is_empty() {
+            writeln!(writer,"{}", 1).unwrap();
+          } else {
+            writeln!(writer,"{}", 0).unwrap();
+          }
+      }
+      // 5. top
+      else if n == "top" {
+          if let Some(&x) = s.last() {
+            writeln!(writer,"{}", x).unwrap();
+          } else {
+            writeln!(writer,"{}", -1).unwrap();
+          }
+      }
+  }
+  writer.flush().unwrap();
 
-        // 1. push
-        if n == "push" {
-            let n2 = nums.next().unwrap().parse::<i32>().unwrap();
-            s.push(n2);
-        }
-        // 2. pop
-        else if n == "pop" {
-            if let Some(x) = s.pop() {
-                println!("{}", x);
-            } else {
-                println!("{}", -1);
-            }
-        }
-        // 3. size
-        else if n == "size" {
-            println!("{}", s.len());
-        }
-        // 4. empty
-        else if n == "empty" {
-            if s.is_empty() {
-                println!("{}", 1);
-            } else {
-                println!("{}", 0);
-            }
-        }
-        // 5. top
-        else if n == "top" {
-            if let Some(&x) = s.last() {
-                println!("{}", x);
-            } else {
-                println!("{}", -1);
-            }
-        }
-    }
+  
 }
