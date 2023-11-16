c2fd327d1481e44ddac35ac28e50064da9e86bc1
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-16 10:48:37 +0900

    [Silver IV] Title: solved.ac, Time: 148 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -1,15 +1,18 @@
-# 다른 사람의 코드
 import sys
- 
+
 def my_round(val):
     return int(val) + 1 if val - int(val) >= 0.5 else int(val)
- 
+
+
 input = sys.stdin.readline
 n = int(input())
+#n이면 종료
 if n:
+#배열에 담고
     arr = [int(input()) for _ in range(n)]
     arr.sort()
     nn = my_round(n * 0.15)
     print(my_round(sum(arr[nn:-nn] if nn else arr) / (n - 2 * nn)))
 else:
     print(0)
+ 
