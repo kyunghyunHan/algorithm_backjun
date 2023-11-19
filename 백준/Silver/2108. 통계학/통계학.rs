648f2e5064240f45d2dc120f76de4977c8bcb54a
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-19 21:30:03 +0900

    [Silver III] Title: 통계학, Time: 84 ms, Memory: 19368 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.rs
+++ 백준/Silver/2108. 통계학/통계학.rs
@@ -1,66 +1,58 @@
-use std::io;
-use std::cmp;
-use std::f64;
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
 use std::collections::HashMap;
 
-fn main() {
-    let mut n = String::new();
-    io::stdin().read_line(&mut n).expect("Failed to read input");
-    let n: usize = n.trim().parse().expect("Invalid input");
+fn main(){
+  let mut reader= BufReader::new(stdin().lock());
+  let mut writer= BufWriter::new(stdout().lock());
+  let mut input= String::new();
+  reader.read_line(&mut input).unwrap();
+  let mut v:Vec<i32>= Vec::new();
+  let  n=  input.trim().parse().unwrap();
 
-    let mut v: Vec<i32> = Vec::new();
-    let mut numv: Vec<(i32, i32)> = Vec::new();
+  for _ in 0..n{
+    input.clear();
+    reader.read_line(&mut input).unwrap();
+    let m:i32 = input.trim().parse().unwrap();
+   v.push(m);
+  }
+  if n==1{
+    writeln!(writer,"{}",v[0]).unwrap();
+    writeln!(writer,"{}",v[0]).unwrap();
+    writeln!(writer,"{}",v[0]).unwrap();
+    writeln!(writer,"{}",0).unwrap();
+    writer.flush().unwrap();
+   return;
+  }
 
-    for _ in 0..n {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read input");
-        let input: i32 = input.trim().parse().expect("Invalid input");
-        v.push(input);
-    }
+  v.sort();
+  //평균
+  let  a= f64::round(v.iter().sum::<i32>() as f64/n as f64 ) as i32;
+  //중앙값 계산
+  let b= v[n/2];
+  let mut num_map: HashMap<i32, i32> = HashMap::new();
+  for i in 0..v.len() {
+      let count = num_map.entry(v[i]).or_insert(0);
+      *count += 1;
+  }
 
-    if n == 1 {
-        let a = v[0];
-        let b = v[0];
-        let c = v[0];
-        let d = 0;
-        println!("{}", a);
-        println!("{}", b);
-        println!("{}", c);
-        println!("{}", d);
-        return;
-    }
+  let mut numv: Vec<(i32, i32)> = num_map.into_iter().collect();
+  numv.sort_by(|a, b| {
+      if a.1 == b.1 {
+          a.0.cmp(&b.0)
+      } else {
+          b.1.cmp(&a.1)
+      }
+  });
 
-    v.sort();
+  let c = if numv[0].1 == numv[1].1 { numv[1].0 } else { numv[0].0 };
 
-    let mut a = 0;
-    for i in 0..n {
-        a += v[i];
-    }
-    a = f64::round(a as f64 / n as f64) as i32;
 
-    let b = v[n / 2];
+  let d = v[n - 1] - v[0];
 
-    let mut num_map: HashMap<i32, i32> = HashMap::new();
-    for i in 0..v.len() {
-        let count = num_map.entry(v[i]).or_insert(0);
-        *count += 1;
-    }
 
-    let mut numv: Vec<(i32, i32)> = num_map.into_iter().collect();
-    numv.sort_by(|a, b| {
-        if a.1 == b.1 {
-            a.0.cmp(&b.0)
-        } else {
-            b.1.cmp(&a.1)
-        }
-    });
-
-    let c = if numv[0].1 == numv[1].1 { numv[1].0 } else { numv[0].0 };
-
-    let d = v[n - 1] - v[0];
-
-    println!("{}", a);
-    println!("{}", b);
-    println!("{}", c);
-    println!("{}", d);
+  writeln!(writer,"{}",a).unwrap();
+  writeln!(writer,"{}",b).unwrap();
+  writeln!(writer,"{}",c).unwrap();
+  writeln!(writer,"{}",d).unwrap();
+  writer.flush().unwrap();
 }
