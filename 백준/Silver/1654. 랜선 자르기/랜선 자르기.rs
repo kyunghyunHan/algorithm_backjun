9ff510e7b960d2f11f4f1dfc772383242c366fb0
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-20 10:08:15 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 13212 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.rs
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.rs
@@ -1,54 +1,65 @@
-use std::cmp::{max, min, Ordering};
-use std::io::{stdin, stdout, BufWriter, prelude::*};
-
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+use std::iter::Iterator;
+//최대 랜선수
 const N_MAX: usize = 1_000_000;
+//최대길이
 const K_MAX: usize = 10_000;
+fn main(){
+  let mut reader= BufReader::new(stdin().lock());
+  let mut writer= BufWriter::new(stdout().lock());
+  let mut input= String::new();
+  reader.read_line(&mut input).unwrap();
+ let mut inputs= input.trim().split_whitespace().map(|x|x.parse::<u64>().unwrap());
+ //랜선의 갯수k
 
-fn main() {
-    solve();
+ //필요한 랜선의 개수n
+ let (k, n) = (inputs.next().unwrap() as usize, inputs.next().unwrap());
+
+let mut nums= [0u64;K_MAX];
+
+//각 랜선의 길이
+for num in nums.iter_mut().take(k){
+  input.clear();
+  reader.read_line(&mut input).unwrap();
+  //랜선의길이
+  *num =input.trim().parse().unwrap();
+  
+}
+let sum: u64 = nums.iter().take(k).sum();
+
+//시작1
+let mut start= 1u64;
+//끝
+let mut end= sum/n+1;
+//최개길이
+let mut max_len=u64::MIN;
+//레이블 사용
+//이진탐색
+'outer: while start < end {
+  //중간길이
+  let middle_len = start + (end - start) / 2;
+  //남은길이
+  let leftover = sum - (middle_len * n);
+  //남은 값들의합
+  let mut leftover_sum = 0u64;
+  for num in nums.iter().take(k) {
+    //각 랜선의 길이를 이용하여 남은 값들의 합을 계산
+    leftover_sum += *num % middle_len;
+    //초과하면
+    if leftover_sum > leftover {
+        end = middle_len;
+        //즉시 다음 레이블로 이동
+        continue 'outer;
+    }
+   
+
+   } 
+    //max_len 값을 업데이트
+    max_len = u64::max(middle_len, max_len);
+    //다음 반복을 위해 start를 조정
+    start = middle_len + 1;
 }
 
-fn solve() {
-    let mut input = String::new();
-    stdin().read_to_string(&mut input).unwrap();
-    let mut input = input.split_ascii_whitespace().flat_map(str::parse::<u64>);
-    
-    // -- test case --
-    // let mut rng = thread_rng();
-    // let mut input: Vec<u64> = vec![K_MAX as u64, rng.gen_range(K_MAX..N_MAX) as u64];
-    // for _ in 0..K_MAX {
-    //     input.push(rng.gen_range(0..2_000_000_000));
-    // }
-    // let mut input = input.into_iter();
+writeln!(writer,"{max_len}").unwrap();
 
-    let (k, n) = (input.next().unwrap() as usize, input.next().unwrap());
-    let mut nums = [0u64; K_MAX];
-    let mut sum = 0u64;
-    for num in nums.iter_mut().take(k) {
-        *num = input.next().unwrap();
-        sum += *num;
-    }
-
-    let mut start = 1u64;
-    let mut end = sum / n + 1;
-    let mut max_len = u64::MIN;
-    'outer: while start < end {
-        let middle_len = start + (end - start) / 2;
-        let leftover = sum - (middle_len * n);
-        let mut leftover_sum = 0u64;
-
-        // can't divide
-        for num in nums.iter().take(k) {
-            leftover_sum += *num % middle_len;
-            if leftover_sum > leftover {
-                end = middle_len;
-                continue 'outer;
-            }
-        }
-        // can divide
-        max_len = max(middle_len, max_len);
-        start = middle_len + 1;
-    }
-
-    println!("{max_len}");
 }
