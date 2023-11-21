ff7fe44539e2d51d75a6c4520fa2f750647c08d3
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-21 09:45:11 +0900

    [Silver II] Title: 스택 수열, Time: 208 ms, Memory: 32704 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,39 @@
+import sys
+
+
+
+# 입력 받기
+n= int(sys.stdin.readline())
+
+cur= 0
+stack=[0]
+answer=[]
+
+#수열 입력받기
+
+for _ in range(0,n):
+    num= int(sys.stdin.readline())
+
+    while cur <num :
+        cur+=1
+        stack.append(cur)
+        answer.append("+")
+
+        if cur ==num:
+            stack.pop()
+            answer.append("-")
+    
+    if cur >num:
+        if stack[len(stack)-1]<num:
+            stack.append(-1)
+            break
+        
+        while stack[len(stack)-1] >=num:
+            stack.pop()
+            answer.append("-")
+
+if len(stack)!=1:
+    print("NO")
+else:
+    for  i in answer:
+        print(i)
