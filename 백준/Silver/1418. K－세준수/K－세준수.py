32f2b26e69bb01fbd851b3426c32ae3e5cfae331
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-11-27 10:48:32 +0900

    [Silver V] Title: K-세준수, Time: 124 ms, Memory: 110024 KB -BaekjoonHub

---
+++ 백준/Silver/1418. K－세준수/K－세준수.py
@@ -0,0 +1,19 @@
+import sys
+
+reader = sys.stdin.readline
+writer = sys.stdout
+
+n = int(reader())
+m = int(reader()) 
+
+s = [0 for i in range(n+1)]
+for i in range(2,n+1):
+    if s[i] == 0:
+        for t in range(i,n+1,i):
+            if t%i == 0:
+                s[t] = max(s[t],i)
+ans = 0
+for i in s:
+    if i <= m:
+        ans += 1
+writer.write(str(ans-1))
