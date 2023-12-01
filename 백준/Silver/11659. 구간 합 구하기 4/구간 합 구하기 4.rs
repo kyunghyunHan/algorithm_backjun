d117739f8a896c58493c2db46777221188e7e89b
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-12-01 09:47:28 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.rs
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.rs
@@ -1,45 +1,45 @@
-use std::io::{stdin, stdout, BufRead, BufReader, Write, BufWriter};
+use std::io::{BufWriter,BufRead,BufReader,Write,stdin,stdout};
 
-fn main() {
-    let stdin = stdin();
-    let stdout = stdout();
-    let mut reader = BufReader::new(stdin.lock());
-    let mut writer = BufWriter::new(stdout.lock());
+fn main(){
+let mut reader= BufReader::new(stdin().lock());
+let mut writer= BufWriter::new(stdout().lock());
 
-    let mut buffer = String::new();
-    reader.read_line(&mut buffer).unwrap();
-    let mut nums = buffer.trim().split_whitespace();
-    let n = nums.next().unwrap().parse::<usize>().unwrap();
-    let m = nums.next().unwrap().parse::<usize>().unwrap();
+let mut input = String::new();
+reader.read_line(&mut input).unwrap();
 
-    let mut v: Vec<usize> = Vec::with_capacity(n);
-    let mut buffer2 = String::new();
-    reader.read_line(&mut buffer2).unwrap();
-    let mut nums2 = buffer2.trim().split_whitespace();
-    for _ in 0..n {
-        let n = nums2.next().unwrap().parse::<usize>().unwrap();
-        v.push(n);
-    }
+let  nums:Vec<usize>= input.trim().split_ascii_whitespace().map(|x|x.parse().unwrap()).collect();
 
-    let mut result_sum = vec![0; n];
-    let mut sum = 0;
-    for (i, &num) in v.iter().enumerate() {
-        sum += num;
-        result_sum[i] = sum;
-    }
+let n = nums[0];
+let m = nums[1];
 
-    for _ in 0..m {
-        let mut buffer3 = String::new();
-        reader.read_line(&mut buffer3).unwrap();
-        let mut nums3 = buffer3.trim().split_whitespace();
-        let n2 = nums3.next().unwrap().parse::<usize>().unwrap();
-        let m2 = nums3.next().unwrap().parse::<usize>().unwrap();
-
-        let result = if n2 == 1 {
-            result_sum[m2 - 1]
-        } else {
-            result_sum[m2 - 1] - result_sum[n2 - 2]
-        };
-        writeln!(writer, "{}", result).unwrap();
-    }
+input.clear();
+reader.read_line(&mut input).unwrap();
+//구간합 저장
+let   list:Vec<i32>= input.trim().split_ascii_whitespace().map(|x|x.parse().unwrap()).collect();
+let mut result_sum = vec![0; n];
+let mut sum = 0;
+for (i, &num) in list.iter().enumerate() {
+    sum += num;
+    result_sum[i] = sum;
 }
+//m개의 줄
+for _ in 0..m{
+    input.clear();
+    reader.read_line(&mut input).unwrap();
+    //ij
+    let ij:Vec<usize>= input.trim().split_ascii_whitespace().map(|x|x.parse().unwrap()).collect();
+    
+    let i = ij[0];
+    let j = ij[1];
+
+    let result = if i == 1 {
+        result_sum[j - 1]
+    } else {
+        result_sum[j - 1] - result_sum[i- 2]
+    };
+    writeln!(writer, "{}", result).unwrap();
+
+}
+writer.flush().unwrap();
+}
+
