2c3485cbc9b70359026e3c11240fcd1b25221ecd
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-12-04 10:12:47 +0900

    [Bronze V] Title: Java Warriors, Time: 0 ms, Memory: 13212 KB -BaekjoonHub

---
+++ 백준/Bronze/30328. Java Warriors/Java Warriors.rs
@@ -0,0 +1,11 @@
+use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
+fn main(){
+    let mut reader= BufReader::new(stdin().lock());
+    let mut writer=BufWriter::new(stdout().lock());
+    let mut input = String::new();
+    reader.read_line(&mut input).unwrap();
+    let n: u32 = input.trim().parse().unwrap();
+
+    writeln!(writer,"{}", 4000 * n).unwrap();
+
+}
