e9a3f67d2f8ce7cf001ccf67357672a210ffb927
kyunhyun94 <ekfqlwcjswl@naver.com>
2023-12-18 09:37:57 +0900

    [Silver II] Title: 좌표 압축, Time: 376 ms, Memory: 37632 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.rs
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.rs
@@ -1,23 +1,24 @@
-use std::io::{stdin, Read};
-
-fn main() {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    let n: usize = input.trim().parse().unwrap();
+use std::io::{BufReader,BufWriter,BufRead,Write,stdin,stdout};
+fn main(){
+    let mut reader= BufReader::new(stdin().lock());
+    let mut writer: BufWriter<std::io::StdoutLock<'_>>= BufWriter::new(stdout().lock());
+    let mut  input= String::new();
+    reader.read_line(&mut input).unwrap();
+    let n:usize= input.trim().parse().unwrap();
 
     input.clear();
-    stdin().read_line(&mut input).unwrap();
+    reader.read_line(&mut input).unwrap();
     let vec: Vec<i32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
-
     let mut copy_vec = vec.clone();
     copy_vec.sort_unstable();
     copy_vec.dedup();
-
     for i in vec {
         let idx = match copy_vec.binary_search(&i) {
             Ok(x) => x,
             Err(x) => x,
         };
-        print!("{} ", idx);
+        write!(writer,"{} ", idx).unwrap();
+
     }
+    writer.flush().unwrap();
 }
