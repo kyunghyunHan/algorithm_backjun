ae1e3be8a1316f132233d138b1000add364c2af8
kyunhyun94 <ekfqlwcjswl@naver.com>
2024-03-17 16:00:23 +0900

    [Bronze III] Title: 터널의 입구와 출구, Time: 116 ms, Memory: 110404 KB -BaekjoonHub

---
+++ 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+f = int(input())
+ans = f
+for _ in range(n):
+    i, j = map(int, input().split())
+    f += (i-j)
+    if f < 0: ans = 0;break
+    ans = max(ans, f)
+print(ans)
