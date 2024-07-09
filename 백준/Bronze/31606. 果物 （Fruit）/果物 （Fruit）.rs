8cbceaa25a1eff2c8a80a6dfbc96db0608e5f528
kyunhyun94 <ekfqlwcjswl@naver.com>
2024-07-09 10:11:33 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 13212 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.rs
@@ -0,0 +1,16 @@
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+
+pub fn main() {
+    let mut reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout().lock());
+    let mut input = String::new();
+
+    reader.read_line(&mut input).unwrap();
+    let a: i32 = input.trim().parse().unwrap();
+    input.clear();
+    reader.read_line(&mut input).unwrap();
+    let b: i32 = input.trim().parse().unwrap();
+
+    writeln!(writer, "{}", a + b + 3).unwrap();
+    writer.flush().unwrap();
+}
