bd2c79a127fc7fe7c47e74040729d2eca997bd3e
kyunhyun94 <ekfqlwcjswl@naver.com>
2024-10-15 21:37:09 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<int> a(n, 0);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+
+    int Result = 0;
+
+    for (int k = 0; k < n; k++)
+    {
+        long find = a[k];
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j)
+        {
+            if (a[i] + a[j] == find)
+            {
+                if (i != k && j != k)
+                {
+                    Result++;
+                    break;
+                }
+                else if (i == k)
+                {
+                    i++;
+                }
+                else if (j == k)
+                {
+                    j--;
+                }
+            }
+            else if (a[i] + a[j] < find)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+    }
+    cout << Result << endl;
+}
