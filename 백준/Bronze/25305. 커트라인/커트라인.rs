a0b7abd0bd796aa61928702b41a21d79533f7f68
kyunhyun94 <ekfqlwcjswl@naver.com>
2024-10-31 22:59:37 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 13216 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.rs
+++ 백준/Bronze/25305. 커트라인/커트라인.rs
@@ -1,21 +1,45 @@
-use std::io;
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+
+fn sort(arr: &mut Vec<i64>, n: usize) {
+    for i in 1..n {
+        let key = arr[i];
+        let mut j = i as isize - 1;
+        while j >= 0 && arr[j as usize] < key {
+            arr[(j + 1) as usize] = arr[j as usize];
+            j -= 1;
+        }
+        arr[(j + 1) as usize] = key;
+    }
+}
 
 fn main() {
-    let mut input_d = String::new();
-    io::stdin().read_line(&mut input_d).unwrap();
-    let v: Vec<i32> = input_d
-        .split_whitespace()
-        .map(|x| -> i32 { x.parse().unwrap() })
-        .collect();
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let mut x: Vec<i32> = input
-        .split_whitespace()
-        .map(|x| -> i32 { x.parse().unwrap() })
-        .collect();
-
-    // x.sort();
-    //거꾸로 정렬
-    x.sort_by(|a, b| b.cmp(a));
-    println!("{:?}", x[v[1] as usize - 1])
+    let reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout().lock());
+    let mut input = reader.lines();
+    
+    if let Some(Ok(line)) = input.next() {
+        let nk = line
+            .trim()
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect::<Vec<i64>>();
+        let n = nk[0] as usize;
+        let k = nk[1] as usize;
+        
+        if let Some(Ok(line)) = input.next() {
+            let mut arr = line
+                .trim()
+                .split_whitespace()
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<i64>>();
+            sort(&mut arr, n);
+            writeln!(writer, "{}", arr[k - 1]).unwrap();
+        } else {
+            writeln!(writer, "Input not defined").unwrap();
+        }
+    } else {
+        writeln!(writer, "Input not defined").unwrap();
+    }
+    
+    writer.flush().unwrap();
 }
