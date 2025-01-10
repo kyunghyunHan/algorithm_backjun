680be3e7a1af2d728124051cdb2844b91d17fe1e
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-01-10 21:47:43 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.rs
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.rs
@@ -1,24 +1,29 @@
-use std::io::{BufReader,BufRead,BufWriter,Write,stdin,stdout};
-fn main(){
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
 
-    let mut reader= BufReader::new(stdin().lock());
-    let mut input = String::new();
-    let mut writer= BufWriter::new(stdout().lock());
-    reader.read_line(&mut input).unwrap();
-   let n :usize= input.trim().parse().unwrap();
-    let mut dp= [0;1001];
-    dp[1]= 1;
-    dp[2]= 3;
-    dp[3]=5;
-    //a1= 1
-    //a2= 2
-    //a3= a1*2+a2
-    //a4 =a2*2+a3
-    //an = (an-2*2) +an-1
-    for i in 4..=n{
-        dp[i]=( dp[i-2]*2)+dp[i-1] ;
-        dp[i]%= 10007;
+fn main() {
+    let reader = BufReader::new(stdin());
+    let mut writer = BufWriter::new(stdout().lock());
+
+    let mut input = reader.lines();
+
+    match input.next() {
+        Some(Ok(line)) => {
+            let n = line.parse::<usize>().unwrap();
+            let mut dp = [0; 1001];
+            dp[1] = 1;
+            dp[2] = 3;
+            dp[3] = 5;
+            for i in 4..=n {
+                // dp[3] = dp[1] +dp[2]
+                dp[i] = (dp[i - 2] * 2) + dp[i - 1];
+                dp[i] %= 10007;
+            }
+
+            writeln!(writer, "{}", dp[n]).unwrap();
+        }
+        _ => {
+            panic!("Error");
+        }
     }
-    writeln!(writer,"{}",dp[n]).unwrap();
     writer.flush().unwrap();
 }
