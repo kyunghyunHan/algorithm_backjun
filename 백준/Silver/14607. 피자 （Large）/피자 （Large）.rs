94d79e1d56ed8cad331cecd8fc24f333ef7b6db3
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-01-26 17:08:06 +0900

    [Silver III] Title: 피자 (Large), Time: 0 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Silver/14607. 피자 （Large）/피자 （Large）.rs
@@ -0,0 +1,15 @@
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+
+fn main() {
+    let mut reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout().lock());
+
+    let mut input = reader.lines();
+
+    if let Some(Ok(line)) = input.next() {
+        let n = line.parse::<usize>().unwrap();
+
+        writeln!(writer, "{}", (n * (n - 1)) / 2).unwrap();
+    }
+    writer.flush().unwrap();
+}
