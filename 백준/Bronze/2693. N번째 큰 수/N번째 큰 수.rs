1235deabbb99a34c061b1c5742357709f97425ad
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-01-29 03:11:58 +0900

    [Bronze I] Title: N번째 큰 수, Time: 0 ms, Memory: 13216 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.rs
@@ -0,0 +1,28 @@
+use std::cmp;
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+fn main() {
+    let mut reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout());
+
+    let mut input = reader.lines();
+    if let Some(Ok(line)) = input.next() {
+        let n = line.parse::<usize>().unwrap();
+
+        for i in 0..n{
+            if let Some(Ok(line)) = input.next() {
+                let mut v = line
+                    .trim()
+                    .split_whitespace()
+                    .map(|x| x.parse().unwrap())
+                    .collect::<Vec<u64>>();
+        
+                v.sort_by(|a, b| b.cmp(a));
+                writeln!(writer, "{}", v[2]).unwrap();
+            }
+        }
+        
+    }
+     
+    
+    writer.flush().unwrap();
+}
