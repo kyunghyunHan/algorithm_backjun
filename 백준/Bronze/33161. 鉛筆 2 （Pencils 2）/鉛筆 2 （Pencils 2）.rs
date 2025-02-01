7cf30019429a5db9d1e5663d33c57f167e0097ff
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-02-01 22:12:49 +0900

    [Bronze V] Title: 鉛筆 2 (Pencils 2), Time: 0 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Bronze/33161. 鉛筆 2 （Pencils 2）/鉛筆 2 （Pencils 2）.rs
@@ -0,0 +1,14 @@
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+
+fn main() {
+    let mut reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout().lock());
+    let mut input = reader.lines();
+
+    if let Some(Ok(line)) = input.next() {
+        let n: u64 = line.parse().unwrap();
+        let result = n / 5;
+        writeln!(writer, "{}", result).unwrap();
+    }
+    writer.flush().unwrap();
+}
