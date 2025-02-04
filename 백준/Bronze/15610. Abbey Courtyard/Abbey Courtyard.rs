f59203b7b01346563bff7badc57b4c4487f74a59
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-02-04 16:48:35 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 0 ms, Memory: 13252 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.rs
@@ -0,0 +1,17 @@
+use std::{
+    cmp::min,
+    io::{stdin, stdout, BufRead, BufReader, BufWriter, Write},
+};
+
+fn main() {
+    let mut reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout().lock());
+
+    let mut input = reader.lines();
+
+    if let Some(Ok(line)) = input.next() {
+        let n = line.parse::<f64>().unwrap();
+        writeln!(writer, "{:?}", n.powf(0.5) * 4.).unwrap();
+    }
+    writer.flush().unwrap();
+}
