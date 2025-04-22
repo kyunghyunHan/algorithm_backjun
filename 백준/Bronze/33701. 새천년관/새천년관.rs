a8f6acf8c308e4f8e9f415a7c453b7e74b9d17c0
kyunhyun94 <ekfqlwcjswl@naver.com>
2025-04-22 09:15:25 +0900

    [Bronze IV] Title: 새천년관, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.rs
@@ -0,0 +1,8 @@
+use std::io::{stdin, stdout, BufRead, BufReader, BufWriter, Write};
+
+fn main() {
+    let mut reader = BufReader::new(stdin().lock());
+    let mut writer = BufWriter::new(stdout().lock());
+
+    writeln!(writer,"nkugwan",).unwrap();
+}
