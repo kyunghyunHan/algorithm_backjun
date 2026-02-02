cbb2c1c033b4d230e4acf83e8a0842f1a98ddaba
kyunhyun94 <ekfqlwcjswl@naver.com>
2026-02-02 22:01:06 +1100

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 1124 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    
+    scanf("%d", &a);
+    
+    if (a >5){
+        printf("%s","Success!");
+    }else{
+        printf("%s","Oh My God!");
+
+    }
+    return 0;
+}
