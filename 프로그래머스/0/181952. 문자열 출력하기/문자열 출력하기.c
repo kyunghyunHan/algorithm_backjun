180f48fc269b395e8d30d68670d9e6aee90e8f1d
kyunhyun94 <ekfqlwcjswl@naver.com>
2026-05-17 21:14:48 +1000

    [level 0] Title: 문자열 출력하기, Time: 3.88 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define LEN_INPUT 1000001
+
+int main(void) {
+
+    char s1[LEN_INPUT] = "HelloWorld!";
+
+    scanf("%s", s1);
+
+    printf("%s\n", s1);
+
+    return 0;
+}
