3e9f10e55f397a0c75dcbac4f7c5b5b9ea54d711
kyunhyun94 <ekfqlwcjswl@naver.com>
2026-08-03 16:35:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 5.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int price) {
+    int answer = 0;
+    price = (float)price;
+    if (price >=100000&&price <300000){
+        answer =price*0.95;
+    }else if(price >=300000&&price <500000){
+        answer =price*0.90;
+
+    }else if(price >=500000){
+        answer =price*0.80;
+    }else {
+             answer = price;
+
+    }
+    return answer;
+}
