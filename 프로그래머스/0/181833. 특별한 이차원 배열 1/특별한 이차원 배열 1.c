ff0b135225cd494cae5dd63fdf5174a44083e3cc
kyunhyun94 <ekfqlwcjswl@naver.com>
2026-08-04 23:36:46 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 1.48 ms, Memory: 6.39 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int** solution(int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int **answer = malloc(sizeof(int *) * n);
+
+for (int i = 0; i < n; i++) {
+    answer[i] = malloc(sizeof(int) * n);
+}
+
+for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        answer[i][j] = (i == j);
+    }
+}
+    return answer;
+}
