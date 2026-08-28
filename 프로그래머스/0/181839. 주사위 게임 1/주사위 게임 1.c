66fac00eb37c1380ac118424c9cfb483d9d74970
kyunhyun94 <ekfqlwcjswl@naver.com>
2026-08-28 22:24:40 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+int solution(int a, int b) {
+    int answer = 0;
+    if(a%2!=0 &&b%2!=0){
+        answer= pow(a,2) + pow(b,2);
+    }else if(a%2!=0||b%2!=0){
+        answer = 2 * (a+b);
+    }else{
+        answer =  abs(a-b);
+    }
+    return answer;
+}
