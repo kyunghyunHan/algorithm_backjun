6633697723c2acfd3897d8cf0dc489ddf9f90d99
kyunhyun94 <ekfqlwcjswl@naver.com>
2026-08-31 11:31:06 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* binomial) {
+    int answer = 0;
+    int a,b;
+    char op;
+    sscanf(binomial, "%d %c %d", &a, &op, &b);
+    
+    if (op == '+') {
+        return a + b;
+    } else if (op == '-') {
+        return a - b;
+    } else {
+        return a * b;
+    }
+    return answer;
+}
