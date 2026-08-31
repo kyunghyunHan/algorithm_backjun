60533f50f13180493aa1039d7510447e2eeb502a
kyunhyun94 <ekfqlwcjswl@naver.com>
2026-08-31 14:34:24 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int* solution(int numbers[], size_t numbers_len, const char* direction) {
+    int* answer = malloc(sizeof(int) * numbers_len);
+
+    if (strcmp(direction, "right") == 0) {
+        answer[0] = numbers[numbers_len - 1];
+
+        for (int i = 0; i < numbers_len - 1; i++) {
+            answer[i + 1] = numbers[i];
+        }
+    }
+    else {
+        for (int i = 1; i < numbers_len; i++) {
+            answer[i - 1] = numbers[i];
+        }
+
+        answer[numbers_len - 1] = numbers[0];
+    }
+
+    return answer;
+}
